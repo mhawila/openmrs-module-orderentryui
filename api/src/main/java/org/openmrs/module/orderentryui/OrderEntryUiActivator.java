994fb95463d8c02cb32ec36afcc63c7b293c23f3
Willa Mhawila <a.mhawila@gmail.com>
2017-10-25 17:07:08 -0500

    ORDER-27: Changed the module activator to populate the order frequencies when it is started

--- api/src/main/java/org/openmrs/module/orderentryui/OrderEntryUiActivator.java
+++ api/src/main/java/org/openmrs/module/orderentryui/OrderEntryUiActivator.java
@@ -18,7 +18,6 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.Concept;
-import org.openmrs.ConceptAnswer;
 import org.openmrs.GlobalProperty;
 import org.openmrs.OrderFrequency;
 import org.openmrs.api.AdministrationService;
@@ -28,21 +27,25 @@ import org.openmrs.api.context.Context;
 import org.openmrs.module.BaseModuleActivator;
 import org.openmrs.util.OpenmrsConstants;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * This class contains the logic that is run every time this module is either started or stopped.
  */
 public class OrderEntryUiActivator extends BaseModuleActivator {
 
-	protected Log log = LogFactory.getLog(getClass());
+    private Log log = LogFactory.getLog(getClass());
 
     @Override
     public void started() {
-        AdministrationService administrationService = Context.getAdministrationService();
-        maybeSetGP(administrationService, OpenmrsConstants.GP_DRUG_ROUTES_CONCEPT_UUID, "162394AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-        maybeSetGP(administrationService, OpenmrsConstants.GP_DRUG_DOSING_UNITS_CONCEPT_UUID, "162384AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-        maybeSetGP(administrationService, OpenmrsConstants.GP_DRUG_DISPENSING_UNITS_CONCEPT_UUID, "162402AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-        maybeSetGP(administrationService, OpenmrsConstants.GP_DURATION_UNITS_CONCEPT_UUID, "1732AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-        ensureOrderFrequencies(Context.getOrderService(), Context.getConceptService(), "160855AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        AdministrationService as = Context.getAdministrationService();
+        String orderFrequencyConvSetUuid = as.getGlobalProperty(OrderEntryUIConstants.GP_ORDER_FREQUENCIES_CONCEPT_SET_UUID);
+        maybeSetGP(as, OpenmrsConstants.GP_DRUG_ROUTES_CONCEPT_UUID, "162394AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        maybeSetGP(as, OpenmrsConstants.GP_DRUG_DOSING_UNITS_CONCEPT_UUID, "162384AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        maybeSetGP(as, OpenmrsConstants.GP_DRUG_DISPENSING_UNITS_CONCEPT_UUID, "162402AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        maybeSetGP(as, OpenmrsConstants.GP_DURATION_UNITS_CONCEPT_UUID, "1732AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        ensureOrderFrequencies(Context.getOrderService(), Context.getConceptService(), orderFrequencyConvSetUuid);
     }
 
     private void maybeSetGP(AdministrationService service, String prop, String val) {
@@ -56,15 +59,41 @@ public class OrderEntryUiActivator extends BaseModuleActivator {
     }
 
     private void ensureOrderFrequencies(OrderService orderService, ConceptService conceptService, String uuid) {
-        if (orderService.getOrderFrequencies(true).size() == 0) {
-            Concept set = conceptService.getConceptByUuid(uuid);
-            if (set != null) {
-                for (ConceptAnswer conceptAnswer : set.getAnswers()) {
-                    Concept concept = conceptAnswer.getAnswerConcept();
-                    if (concept != null) {
-                        OrderFrequency frequency = new OrderFrequency();
-                        frequency.setConcept(concept);
-                        orderService.saveOrderFrequency(frequency);
+
+        List<OrderFrequency> existing = orderService.getOrderFrequencies(true);
+        Concept frequencySetConcept = null;
+        if (StringUtils.isNotEmpty(uuid)) {
+            frequencySetConcept = conceptService.getConceptByUuid(uuid);
+        }
+
+        if (existing.size() == 0 && (StringUtils.isEmpty(uuid) || frequencySetConcept == null)) {
+            log.error("The global property " + OrderEntryUIConstants.GP_ORDER_FREQUENCIES_CONCEPT_SET_UUID +
+                    "value " + uuid + " is not valid");
+        }
+
+        if (frequencySetConcept != null) {
+            List<Concept> members = frequencySetConcept.getSetMembers();
+            if (members != null && members.size() > 0) {
+                // add only missing members.
+                List<Concept> alreadyWithFrequency = new ArrayList<Concept>();
+                for (OrderFrequency frequency : existing) {
+                    alreadyWithFrequency.add(frequency.getConcept());
+                }
+
+                List<Concept> toadd = new ArrayList<Concept>();
+                if(alreadyWithFrequency.size() > 0) {
+                    for(Concept concept: members) {
+                        if(!alreadyWithFrequency.contains(concept)) {
+                            toadd.add(concept);
+                        }
+                    }
+                }
+
+                for (Concept member : toadd) {
+                    if (!member.isRetired()) {
+                        OrderFrequency orderFrequency = new OrderFrequency();
+                        orderFrequency.setConcept(member);
+                        orderService.saveOrderFrequency(orderFrequency);
                     }
                 }
             }
